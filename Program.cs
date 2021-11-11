1518e6d3320a5d2fb5bb4d42918d9760d1bcf053
Putseyeu <putseyeu.a.d@gmail.com>
2021-11-11 14:21:49 +0100

    step1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorkCycl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // используем цикл for, оптимален при условии что есть чётки границы цикла.  
+            for( int i = 7; i < 100; i += 7)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
